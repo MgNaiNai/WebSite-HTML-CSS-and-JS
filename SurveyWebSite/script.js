9e404fd8c9d8c18bcf4dcce850fd77816c7d2dd5
MgNaiNai <121297414+MgNaiNai@users.noreply.github.com>
2024-04-19 22:54:35 +0630

    Delete SurveyWebSite directory

--- SurveyWebSite/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('survey-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // You can add your submission handling logic here
-    alert('Form submitted successfully!');
-  });
-  
