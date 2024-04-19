d22524f386a7f0e7fd8accfec3e161fa0adb5930
MgNaiNai <121297414+MgNaiNai@users.noreply.github.com>
2024-04-19 22:52:13 +0630

    Add files via upload

---
+++ SurveyWebSite/script.js
@@ -0,0 +1,6 @@
+document.getElementById('survey-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add your submission handling logic here
+    alert('Form submitted successfully!');
+  });
+  
